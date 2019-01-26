49d81124c1dc87189d3edc69a34daa0261e87be8
Michael Lange <dingoeatingfuzz@gmail.com>
2019-01-25 23:32:04 -0800

    Fix the broken syntax for constructing the map

--- Assets/Scripts/GameData.cs
+++ Assets/Scripts/GameData.cs
@@ -4,34 +4,28 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameData : MonoBehaviour {
-  static public ScreenGraph Map = new ScreenGraph {
+  static public ScreenGraph Map = new ScreenGraph() {
     { "FrontDoor",
       new ScreenNode("FrontDoor", new List<ScreenExit> {
-        {
-          new ScreenExit { Id = "Left", To = "CircleKey", Exit = "Right" },
-          new ScreenExit { Id = "Right", To = "DoubleJump", Exit = "Left" }
-        }
+        new ScreenExit { Id = "Left", To = "CircleKey", Exit = "Right" },
+        new ScreenExit { Id = "Right", To = "DoubleJump", Exit = "Left" }
       })
     },
     { "CircleKey",
       new ScreenNode("CircleKey", new List<ScreenExit> {
-        {
-          new ScreenExit { Id = "Left", To = "DoubleJump", Exit = "Right" },
-          new ScreenExit { Id = "Right", To = "DoubleJump", Exit = "Left" }
-        }
+        new ScreenExit { Id = "Left", To = "DoubleJump", Exit = "Right" },
+        new ScreenExit { Id = "Right", To = "DoubleJump", Exit = "Left" }
       })
     },
     { "DoubleJump",
       new ScreenNode("DoubleJump", new List<ScreenExit> {
-        {
-          new ScreenExit { Id = "Left", To = "CircleKey", Exit = "Right" },
-          new ScreenExit { Id = "Right", To = "FrontDoor", Exit = "Left" }
-        }
+        new ScreenExit { Id = "Left", To = "CircleKey", Exit = "Right" },
+        new ScreenExit { Id = "Right", To = "FrontDoor", Exit = "Left" }
       })
     },
   };
 
-  static public GoToExit(string To, string Exit) {
+  static public void GoToScreen(string To, string Exit) {
     var node = GameData.Map[To];
     // Go to the matching scene
     SceneManager.LoadScene(To);
