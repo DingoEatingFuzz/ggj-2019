90051803beae5d47e6f8045124306646267244bd
ADKauth <adamkauth@gmail.com>
2019-01-26 01:53:28 -0800

    Make camera follow Gina

---
+++ Assets/Scripts/CamFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFollow : MonoBehaviour
+{
+    public Transform charLocation;
+    private float distanceFromChar = 15.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = charLocation.position + new Vector3(0, 2, -distanceFromChar);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 toPos = charLocation.position + new Vector3(0, 2, -distanceFromChar);
+        transform.position = toPos;
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, charLocation.rotation, Time.deltaTime * 5);
+
+    }
+}
