bd4a49d1657a9beaf5aa0cb1636913c7cfb630b4
Michael Lange <dingoeatingfuzz@gmail.com>
2019-01-27 11:07:41 -0800

    Fancy ScxreenElement prefab

---
+++ Assets/Scripts/ScreenElement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ScreenElement : MonoBehaviour
+{
+    public Sprite sprite;
+    public bool isFloor = false;
+    public int width = 1;
+    public int height = 1;
+
+    void Awake()
+    {
+        Debug.Log("Calling awake");
+    }
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        var s = new Vector2(width, height);
+        SpriteRenderer spriteComp = this.GetComponent<SpriteRenderer>();
+        spriteComp.sprite = sprite;
+        spriteComp.size = s;
+
+        BoxCollider2D colliderComp = this.GetComponent<BoxCollider2D>();
+        colliderComp.size = s;
+
+        this.tag = isFloor ? "Ground" : "Untagged";
+    }
+}
